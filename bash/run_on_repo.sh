a102d0ef06d45e92b753e92aa64b5229f3d4511c
bkushigian <bkushigian@gmail.com>
2023-10-06 23:34:58 -0700

    Used HOME instead of ~

--- bash/run_on_repo.sh
+++ bash/run_on_repo.sh
@@ -9,7 +9,7 @@ shift
 
 REPO_NAME=${REPO##*/}
 
-cd ~
+cd "$HOME"
 echo "Cloning $PROJECT_NAME from $REPO to $REPO_NAME"
 git clone "$REPO"
 cd "$REPO_NAME"
@@ -17,12 +17,12 @@ npm install
 echo "Running run_on_hardhat on sourceroots '$@'"
 run_on_hardhat . "$@" --collect_data --solc "$SOLC" --solang_parser
 
-echo "Concatenate summary.md to ~/summary.md"
+echo "Concatenate summary.md to $HOME/summary.md"
 [ ! -e summary.md ] && { echo "summary.md does not exist"; exit 1; }
-echo "" >> ~/summary.md
-echo "## $PROJECT_NAME Summary" >> ~/summary.md
-echo "" >> ~/summary.md
-cat summary.md >> ~/summary.md
+echo "" >> "$HOME/summary.md"
+echo "## $PROJECT_NAME Summary" >> "$HOME/summary.md"
+echo "" >> "$HOME/summary.md"
+cat summary.md >> "$HOME/summary.md"
 
 echo "Moving summary.json to $HOME/${REPO_NAME}_summary.json"
 [ ! -e summary.json ] && { echo "summary.json does not exist"; exit 1; }
